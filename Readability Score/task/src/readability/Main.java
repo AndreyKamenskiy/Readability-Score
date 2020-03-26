94e6d5fc5cc98e1246e161e052ec01f6c198693b
Andrey Kamenskiy <52415618+AndreyKamenskiy@users.noreply.github.com>
2020-03-27 00:11:32 +0300

    stage 2 complete.

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -7,7 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        if (text.length() > 100) {
+        int sentences = text.split("[.!?]\\s*").length;
+        int words = text.trim().split("\\s+").length;
+        //System.out.printf("sentences=%d words=%d average=%.2f \n", sentences, words, ((float) words) / sentences );
+        if (((float) words) / sentences > 10.0f) {
             System.out.print("HARD");
         } else {
             System.out.print("EASY");
