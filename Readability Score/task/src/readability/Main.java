82e5328ca19f4364369d2aa4aa2bab83a6fca0d2
Andrey Kamenskiy <52415618+AndreyKamenskiy@users.noreply.github.com>
2020-03-28 17:41:12 +0300

    comment delete

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -81,54 +81,5 @@ public class Main {
             default:
                 System.out.println("Unknown choice");
         }
-
-/*        String age = null;
-        switch ((int) Math.ceil(score)) {
-            case 1:
-                age = "5-6";
-                break;
-            case 2:
-                age = "6-7";
-                break;
-            case 3:
-                age = "7-9";
-                break;
-            case 4:
-                age = "9-10";
-                break;
-            case 5:
-                age = "10-11";
-                break;
-            case 6:
-                age = "11-12";
-                break;
-            case 7:
-                age = "12-13";
-                break;
-            case 8:
-                age = "13-14";
-                break;
-            case 9:
-                age = "14-15";
-                break;
-            case 10:
-                age = "15-16";
-                break;
-            case 11:
-                age = "16-17";
-                break;
-            case 12:
-                age = "17-18";
-                break;
-            case 13:
-                age = "18-24";
-                break;
-            case 14:
-                age = "24+";
-                break;
-            default:
-                age = "unknown_" + score;
-        }
-        System.out.printf("This text should be understood by %s year olds.", age); */
     }
 }
